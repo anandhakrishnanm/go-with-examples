febc65807bd617a32549843d0ee18fb8ab71df3a
Anandhakrishnan M <anandhakrishnanmohank@gmail.com>
2023-03-25 15:21:32 +0530

    initial commit

---
+++ 18strings_and_runes/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
+func main(){
+	const s = "สวัสดี"
+	fmt.Println("Len:", len(s))
+
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("%x ", s[i])
+	}
+	fmt.Println()
+
+	fmt.Println("Roune Count:", utf8.RuneCountInString(s))
+
+	for idx, runeValue := range s {
+		fmt.Printf("%#U start at %d\n", runeValue, idx)
+	}
+
+	fmt.Println("\nUsing DecodeRuneInString")
+	for i, w := 0, 0; i < len(s); i += w {
+		runeValue, width := utf8.DecodeRuneInString(s[i:])
+		fmt.Printf("%#U start at %d\n", runeValue, i)
+		w = width
+		exaineRune(runeValue)
+	}
+}
+
+func exaineRune(r rune) {
+	if r == 't' {
+        fmt.Println("found tee")
+    } else if r == 'ส' {
+        fmt.Println("found so sua")
+    }
+}
